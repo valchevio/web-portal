5d240ea4c70d26a1f92075b58f814ef8e607f01e
Antoan Valchev <antoan@valchev.io>
2025-03-09 21:52:27 +0100

    Update Alpine JS CDN.

--- src/Views/home.php
+++ src/Views/home.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tasks Portal</title>
-    <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.13.2/dist/cdn.min.js"></script>
+    <script src="//unpkg.com/alpinejs" defer></script>
     <script src="https://cdn.tailwindcss.com"></script>
     <style>
         [x-cloak] {
